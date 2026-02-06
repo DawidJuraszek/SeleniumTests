3d109ad9314b3f709093a737f6494e7bb01f31a7
Dawid Juraszek <dawidjuraszek90@gmail.com>
2026-02-06 18:20:13 +0100

    third commit

---
+++ src/test/java/IframeTest.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class IframeTest {
+
+
+    @Test
+    public void testNewWindow() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://testeroprogramowania.github.io/selenium/iframe.html");
+        WebElement iframe = driver.findElement(By.cssSelector("[src='basics.html']"));
+        driver.switchTo().frame(iframe);
+        driver.findElement(By.id("fname")).sendKeys("Dawid");
+        driver.switchTo().defaultContent();
+        System.out.println(driver.findElement(By.tagName("h1")).getText());
+    }
+}
