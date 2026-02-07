9e485d91943e684bef1a30f7a5fb01171d893d14
Dawid Juraszek <dawidjuraszek90@gmail.com>
2026-02-07 07:40:06 +0100

    fourth commit

---
+++ src/test/java/ExecutorTest.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class ExecutorTest {
+
+    @Test
+    public void executeJavaScript() {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://testeroprogramowania.github.io/selenium/");
+        WebElement basicPageLink = driver.findElement(By.linkText("Podstawowa strona testowa"));
+        JavascriptExecutor executor = (JavascriptExecutor) driver;
+        executor.executeScript("arguments[0].click();",basicPageLink);
+
+        WebElement firstname = driver.findElement(By.name("fname"));
+        executor.executeScript("arguments[0].setAttribute('value','Dawid');",firstname);
+    }
+}
---
+++ src/test/java/UploadTest.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class UploadTest {
+
+    @Test
+    public void uploadFile() {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://testeroprogramowania.github.io/selenium/fileupload.html");
+        driver.findElement(By.id("myFile")).sendKeys("/Users/dawidjuraszek/Desktop/conduit .xml");
+    }
+
+
+}
