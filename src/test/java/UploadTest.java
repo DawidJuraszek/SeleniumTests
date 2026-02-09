2ce511bdc750476175a3abec17131e53b0de0373
Dawid Juraszek <dawidjuraszek90@gmail.com>
2026-02-09 06:27:56 +0100

    sixth commit

--- src/test/java/UploadTest.java
+++ src/test/java/UploadTest.java
@@ -1,16 +1,28 @@
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import java.io.File;
+import java.io.IOException;
+
 public class UploadTest {
 
     @Test
-    public void uploadFile() {
+    public void uploadFile() throws IOException {
 
         WebDriver driver = new ChromeDriver();
         driver.get("https://testeroprogramowania.github.io/selenium/fileupload.html");
+        TakesScreenshot screenshot = (TakesScreenshot) driver;
+        File before = screenshot.getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(before,new File("src/test/resources/beforeUpload.png"));
         driver.findElement(By.id("myFile")).sendKeys("/Users/dawidjuraszek/Desktop/conduit .xml");
+        File after = screenshot.getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(after,new File("src/test/resources/afterUpload.png"));
+
 
     }
 
